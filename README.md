32d87475ff385a43ae40f0bed1175a4bd2af24e2
tsv-stacks <tsvirdi@yahoo.co.uk>
2023-02-04 22:29:24 +0000

    added readme and test

---
+++ README.md
@@ -0,0 +1,21 @@
+convert calculation.js to class constructor
+
+html/css
+project code date analyst
+
+modal
+agar spread/pour
+batch code
+date & times
+indicator
+no. of samples single, duplicate, triplicate(default)
+
+calculate split into:
+
+1. input cleaning to usable format
+2. input to results
+3. results to logs
+
+future
+
+1. custom dilutions and calculations
--- __tests__/input.test.js
+++ __tests__/input.test.js
@@ -3,6 +3,11 @@ const { Input } = require('../scripts/input.js')
 describe("Input class", () => {
     let input;
 
+    it("can be instantiated", () => {
+        input = new Input([])
+        expect(input).toBeInstanceOf(Object);
+    });
+
     it("replaces empty strings with 0", () => {
         input = new Input([1, 2, "", undefined, null, 3]);
         expect(input.replaceEmptyWithZero()).toEqual([1, 2, 0, 0, 0, 3]);
